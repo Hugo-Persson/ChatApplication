94ff94d830a40bc72bd802e5bb27b7a0544a447f
Hugo Persson <hugopersson7@outlook.com>
2019-10-09 20:26:58 +0200

    made it possible to sign up

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const mongodb = require("mongodb");
+
+const secretJSONToken = "faa807c4-d05e-47f3-91af-e09ec6cb80ce";
+const con = "mongodb+srv://Hugo:ecGXESd8L9zdfi3@cluster0-g63l9.mongodb.net/test?retryWrites=true&w=majority";
 
 
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+async function getUserCol() {
+    const connect = await mongodb.connect(con, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const db = await connect.db("chatapp");
+    const col = db.collection("users");
+    return col;
+
+}
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -21,3 +44,36 @@ io.on("connection", (socket) => {
 });
 
 http.listen(4200, () => console.log("4200"));
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/register.html");
+});
+app.get("/css/auth.css", (req, res) => {
+    res.sendFile(__dirname + "/css/auth.css");
+});
+app.post("/registerUser", async (req, res) => {
+    try {
+        let user = req.body;
+        const col = await getUserCol();
+        const hashedPassword = await hashPassword(user.password);
+        user.password = hashedPassword;
+        console.log(user);
+        await col.insertOne(user);
+        res.send("success");
+    } catch (err) {
+        console.log(err);
+        res.send("error");
+    }
+
+
+});
+
+async function hashPassword(password) {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    } catch (err) {
+        console.log(err);
+    }
+
+}
